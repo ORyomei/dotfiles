85bed834aaac450559315de0e720c7db13226aa3
ORyomei <o.ryomei1020@gmail.com>
2025-06-30 02:47:15 +0900

    add eza install script

--- install.sh
+++ install.sh
@@ -4,5 +4,13 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 ln -sfnv "${SCRIPT_DIR}"/.zshrc ~
 ln -sfnv "${SCRIPT_DIR}"/.config/ ~
 
-curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
-    | bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
+curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh |
+    bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
+
+sudo apt install -y gpg
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza

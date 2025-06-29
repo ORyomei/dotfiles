9f258665d2568b9963c5f97132af04ff44ef74cc
ORyomei <o.ryomei1020@gmail.com>
2025-06-30 02:43:27 +0900

    add sheldon install script

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 ln -sfnv "${SCRIPT_DIR}"/.zshrc ~
 ln -sfnv "${SCRIPT_DIR}"/.config/ ~
+
+curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
+    | bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
